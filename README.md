fc7f3f04e908bcbcf60d9097197b02f2844eb5ae
Ganesh Ravichandran <gr2483@columbia.edu>
2016-07-18 15:55:46 -0400

    made into npm module

--- README.md
+++
@@ -1,6 +0,0 @@
-# gbfs2geojson-js
-NPM module to convert GBFS feeds to geojson
-
-
-## License
-This repository is licensed under the [Creative Commons Attribution 3.0 License](http://creativecommons.org/licenses/by/3.0/).
--- index.js
+++ index.js
@@ -1,114 +1,137 @@
-var parseGBFS = function(statusURL, alertURL, infoURL) {
-  var json = {
-    type: "FeatureCollection",
-    features: []
-  }
-  // list of stations indexed by station_id, organized like the JSON feature object
-  var feature = {};
-    $.ajax(statusURL, function(data, statusText) {
-      json1 = false;
-      
-      var stations = data.data.stations;
-      
-      for (station in stations) {
-        
-        if (!feature[stations[station].station_id]) {
-          feature[stations[station].station_id]={
-              properties: {}
-            }
-        };
-        
-        feature[stations[station].station_id].properties = $.extend(feature[stations[station].station_id].properties, stations[station]);
-      }
-      
-      json1 = true;
-    });
+var request = require('request');
 
-    // Get system alerts asynchronously
-    $.getJSON(alertURL, function(data, statusText) {
-      json2 = false;
-      var alerts = data.data.alerts;
-      for (alert in alerts) {
-        var alertStations = alerts[alert].station_ids;
-        for (station in alertStations) {
-          
-          if (!feature[alertStations[station]]) {
-            feature[alertStations[station]]={
-              properties: {}
-            }
-          };
-          
-          feature[alertStations[station]].properties = $.extend(feature[alertStations[station]].properties, alerts[alert]);
-        }
-      }
-      json2 = true;
-    });
-    
-    // Get station information asynchronously. This contains geog info, so that is inserted here too
-    $.getJSON('infoURL', function(data, statusText) {
-      json3 = false;
-      var stations = data.data.stations;
-      for (station in stations) {
-        
-        if (!feature[stations[station].station_id]) {
-          feature[stations[station].station_id]={
+module.exports = {
+
+  featureFromProperties: function(feature, featureProperties) {
+
+    // if alert.station_ids
+    if (featureProperties.station_ids) {
+      featureProperties.station_ids.forEach(function(station_id){
+        if (!feature[station_id]) {
+          feature[station_id]={
             properties: {}
           }
         };
         
-        feature[stations[station].station_id].properties = $.extend(feature[stations[station].station_id].properties, stations[station]);
-        feature[stations[station].station_id].type = "Feature";
-        feature[stations[station].station_id].geometry = {
-          type: 'Point',
-          coordinates: [
-            stations[station].lat,
-            stations[station].lon
-          ]
-        };
-      }
-      json3 = true;
-    });
-
-  // Get station information asynchronously. This contains geog info, so that is inserted here too
-  $.getJSON('http://api-core.citibikenyc.com/gbfs/en/station_information.json', function(data, statusText) {
-    var stations = data.data.stations;
-    for (station in stations) {
-      
-      // If this is the first function to execute, create the object for the station
-      if (!feature[stations[station].station_id]) {
-        feature[stations[station].station_id] = {
+        module.exports.updateFeature(feature, station_id, featureProperties);
+      });
+    }
+    // if station
+    else if (featureProperties.station_id) {
+      if (!feature[featureProperties.station_id]) {
+        feature[featureProperties.station_id] = {
           properties: {}
         }
       };
-      
-      feature[stations[station].station_id].properties = $.extend(feature[stations[station].station_id].properties, stations[station]);
-      
-      // Also, add any additional geoJSON required information.
-      feature[stations[station].station_id].type = "Feature";
-      feature[stations[station].station_id].geometry = {
-        type: 'Point',
-        coordinates: [
-          stations[station].lat,
-          stations[station].lon
-        ]
-      };
+      module.exports.updateFeature(feature, featureProperties.station_id, featureProperties);
+
     }
-  });
-
-  // After ajax completion
-  $(document).on("ajaxStop", function() {
-    // remove any pins on the pin layer
-
-    for (obj in feature) {
-      json.features.push(obj)
-    }
-    console.log(JSON.stringify(json));
-    return json;
-  });
-}
-
-module.exports = {
-  parseGBFS: function(statusURL, alertURL, infoURL){
-    parseGBFS(statusURL, alertURL, infoURL)
   },
+
+  updateFeature: function(feature, station_id, feature_properties) {
+
+    for(var key in feature_properties) {
+      if(feature_properties.hasOwnProperty(key)) {
+        feature[station_id].properties[key] = feature_properties[key];
+      }
+    }
+  },
+
+  featureFromInformation: function(feature, station) {
+    module.exports.featureFromProperties(feature, station);
+    feature[station.station_id].type = "Feature";
+    feature[station.station_id].geometry = {
+      type: 'Point',
+      coordinates: [
+        station.lat,
+        station.lon
+      ]
+    };
+  },
+
+  getStationStatus: function(feature, url, callback) {
+
+    request({
+      url: url,
+      json: true
+    }, function(error, response, data) {
+      
+      var stations = data.data.stations;
+
+      for (var stationIndex in stations) {
+        var stationProperties = stations[stationIndex];
+        module.exports.featureFromProperties(feature, stationProperties);
+      }
+      
+      callback(feature);
+
+    });
+  },
+
+  getSystemAlerts: function(feature, url, callback) {
+    // Get system alerts asynchronously
+    request({
+      url: url,
+      json: true
+    }, function(error, response, data) {
+      var alerts = data.data.alerts;
+
+      for (var alertIndex in alerts) {
+        var alertProperties = alerts[alertIndex];
+        module.exports.featureFromProperties(feature, alertProperties);
+      }
+
+      callback(feature);
+
+    });
+  },
+
+  getStationInformation: function(feature, url, callback) {
+    // Get station information asynchronously. This contains geog info, so that is inserted here too
+    request({
+      url: url,
+      json: true
+    }, function(error, response, data) {
+      var stations = data.data.stations;
+
+      for (var stationIndex in stations) {
+        var stationProperties = stations[stationIndex];
+        module.exports.featureFromInformation(feature, stationProperties);
+      }
+
+      callback(feature);
+
+    });
+  },
+
+  getAllData: function(data, callback) {
+
+    module.exports.getStationInformation(data, 'http://api-core.citibikenyc.com/gbfs/en/station_information.json',
+      function(data) {
+
+        module.exports.getStationStatus(data, 'http://api-core.citibikenyc.com/gbfs/en/station_status.json',
+          function(data) {
+
+            module.exports.getSystemAlerts(data, 'http://api-core.citibikenyc.com/gbfs/en/system_alerts.json',
+              function(data) {
+
+                callback(data);
+              });
+          });
+      });
+  }
+
+  getStatusAndAlerts: function(data, callback) {
+
+    module.exports.getStationStatus(data, 'http://api-core.citibikenyc.com/gbfs/en/station_status.json',
+      function(data) {
+
+        module.exports.getSystemAlerts(data, 'http://api-core.citibikenyc.com/gbfs/en/system_alerts.json',
+          function(data) {
+
+            callback(data);
+          });
+      });
+  }
+
 }
